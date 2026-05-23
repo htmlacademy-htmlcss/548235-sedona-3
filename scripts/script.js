ab2c5275269b847fd1abde7d1b01fb50c30201cd
adjisay <pe_1n@mail.ru>
2026-05-23 16:17:06 +0300

    feat: исправить все недочеты по критериям (fix all issues according to criteria)

--- scripts/script.js
+++ scripts/script.js
@@ -1,23 +1,23 @@
-const modal = document.querySelector('.modal')
-const openButton = document.querySelector('.hotel-search__button')
-const closeButton = document.querySelector('.modal__close-button')
+const modal = document.querySelector(".modal")
+const openButton = document.querySelector(".hotel-search__button")
+const closeButton = document.querySelector(".modal__close-button")
 
-openButton.addEventListener('click', () => {
-  modal.classList.remove('modal--hidden')
+openButton.addEventListener("click", () => {
+  modal.classList.remove("modal--hidden")
 })
 
-closeButton.addEventListener('click', () => {
-  modal.classList.add('modal--hidden')
+closeButton.addEventListener("click", () => {
+  modal.classList.add("modal--hidden")
 })
 
-modal.addEventListener('click', (e) => {
+modal.addEventListener("click", (e) => {
   if (e.target === modal) {
-    modal.classList.add('modal--hidden')
+    modal.classList.add("modal--hidden")
   }
 })
 
-document.addEventListener('keydown', (e) => {
-  if (e.key === 'Escape' && !modal.classList.contains('modal--hidden')) {
-    modal.classList.add('modal--hidden')
+document.addEventListener("keydown", (e) => {
+  if (e.key === "Escape" && !modal.classList.contains("modal--hidden")) {
+    modal.classList.add("modal--hidden")
   }
 })
